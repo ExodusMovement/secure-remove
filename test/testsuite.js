e789710b210a11cb9f7fb757bbd2c06820157408
Сковорода Никита Андреевич <chalkerx@gmail.com>
2021-02-10 23:54:58 +0300

    refactor: remove testsuite function

--- test/testsuite.js
+++ test/testsuite.js
@@ -4,48 +4,44 @@ const fs = require('fs-extra')
 const secureRemove = require('..')
 const tmp = require('tempfile')
 
-function testsuite () {
-  test(`file not exists`, async (t) => {
-    const tempfile = tmp()
+test(`file not exists`, async (t) => {
+  const tempfile = tmp()
 
-    await secureRemove(tempfile).catch((err) => {
-      t.true(err instanceof Error)
-      t.equal(err.message, `ENOENT: no such file or directory, stat '${tempfile}'`)
-    })
+  await secureRemove(tempfile).catch((err) => {
+    t.true(err instanceof Error)
+    t.equal(err.message, `ENOENT: no such file or directory, stat '${tempfile}'`)
   })
+})
 
-  test(`options.iterations is 1, as result data is not same`, async (t) => {
-    const tempfile = tmp()
-    let dataOriginal = randomBytes(1024)
-    await fs.writeFile(tempfile, dataOriginal)
-    await secureRemove(tempfile, { iterations: 1, randomSource: '/dev/urandom' })
-    const data = await fs.readFile(tempfile)
-    t.notEqual(dataOriginal.toString('hex'), data.toString('hex'))
-  })
+test(`options.iterations is 1, as result data is not same`, async (t) => {
+  const tempfile = tmp()
+  let dataOriginal = randomBytes(1024)
+  await fs.writeFile(tempfile, dataOriginal)
+  await secureRemove(tempfile, { iterations: 1, randomSource: '/dev/urandom' })
+  const data = await fs.readFile(tempfile)
+  t.notEqual(dataOriginal.toString('hex'), data.toString('hex'))
+})
 
-  test(`options.size = 42K`, async (t) => {
-    const tempfile = tmp()
-    await fs.writeFile(tempfile, randomBytes(42))
-    await secureRemove(tempfile, { size: '42K' })
-    const stat = await fs.stat(tempfile)
-    t.equal(stat.size, 42 * 1024)
-  })
+test(`options.size = 42K`, async (t) => {
+  const tempfile = tmp()
+  await fs.writeFile(tempfile, randomBytes(42))
+  await secureRemove(tempfile, { size: '42K' })
+  const stat = await fs.stat(tempfile)
+  t.equal(stat.size, 42 * 1024)
+})
 
-  test(`options.remove is true`, async (t) => {
-    const tempfile = tmp()
-    await fs.writeFile(tempfile, randomBytes(1024))
-    await secureRemove(tempfile, { remove: true })
-    const exists = await fs.pathExists(tempfile)
-    t.false(exists, 'file should not exist')
-  })
+test(`options.remove is true`, async (t) => {
+  const tempfile = tmp()
+  await fs.writeFile(tempfile, randomBytes(1024))
+  await secureRemove(tempfile, { remove: true })
+  const exists = await fs.pathExists(tempfile)
+  t.false(exists, 'file should not exist')
+})
 
-  test(`options.exact and options.zero is true`, async (t) => {
-    const tempfile = tmp()
-    await fs.writeFile(tempfile, randomBytes(1024))
-    await secureRemove(tempfile, { exact: true, zero: true })
-    const data = await fs.readFile(tempfile)
-    t.equal(data.toString('hex'), Buffer.alloc(1024).toString('hex'))
-  })
-}
-
-testsuite()
+test(`options.exact and options.zero is true`, async (t) => {
+  const tempfile = tmp()
+  await fs.writeFile(tempfile, randomBytes(1024))
+  await secureRemove(tempfile, { exact: true, zero: true })
+  const data = await fs.readFile(tempfile)
+  t.equal(data.toString('hex'), Buffer.alloc(1024).toString('hex'))
+})
